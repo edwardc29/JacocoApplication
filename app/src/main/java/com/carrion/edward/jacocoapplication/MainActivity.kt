69d26bd5c2da491dfc2830dc62a0ed397454c4fb
Edward Carrion <edwardcarrionp@bcp.com.pe>
2020-04-08 22:02:49 -0500

    bug: add codsmell for testing

--- app/src/main/java/com/carrion/edward/jacocoapplication/MainActivity.kt
+++ app/src/main/java/com/carrion/edward/jacocoapplication/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val myUnusedVariable = 1
+        val codeSmell = 0
     }
 }
